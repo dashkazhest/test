a83269b9a08abf760bd26fede0ce49b1a608cba6
dashkazhest <ebuchka723@gmail.com>
2026-02-21 21:03:50 +0700

    add comments

--- cpp_program/socket_server.cpp
+++ cpp_program/socket_server.cpp
@@ -15,24 +15,27 @@ using namespace std;
 
 int main() {
 
+    //создание сокета
     int sock = socket (AF_UNIX, SOCK_STREAM, 0);
     if (sock < 0) {
         cerr << "socket error" << endl;
         return 1;
     }
 
+    //установление адреса - путь к файлу
     struct sockaddr_un addr;
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, "/tmp/unix_sock", sizeof(addr.sun_path)-1);
 
-    unlink("/tmp/unix_sock");
-    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr))<0){
+    unlink("/tmp/unix_sock"); // удаление сокета с прошлого запуска
+    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr))<0){ // привязка адреса
         cerr << "bind error" << endl;
         return 1;
     }
 
-    if(listen(sock,1) <0){
+    //прослушивание подключений клиента и ожидание
+    if(listen(sock,1) <0){ 
         cerr << "listen error" << endl;
         return 1;
     };
@@ -43,6 +46,7 @@ int main() {
         return 1;
     }
 
+    //принятие данных от программы-клиента
     char buf[64]; 
     memset(buf, 0, sizeof(buf));
     int message = recv(client, buf, sizeof(buf)-1, 0);
@@ -52,6 +56,7 @@ int main() {
     } 
     cout << buf << endl;
 
+    //логирование с подписью даты и времени получения
     time_t now = time(0);
     char timebf[64];
     strftime(timebf, sizeof(timebf), "%d-%m-%Y %H:%M", localtime(&now));
@@ -59,10 +64,11 @@ int main() {
     log << "data: " << buf << " [ date and time of receipt: " << timebf << "]"<< endl;
     log.close();
 
+    // отправка данных программе-клиенту
     char text[64] = "SOCKET_SERVER_ON_CPP_HI";
     send(client, text, strlen(text), 0);
 
-    close(client);
+    close(client); // закрытие сокетов
     close(sock);
     return 0;
 }
--- rust_program/src/main.rs
+++ rust_program/src/main.rs
@@ -9,16 +9,21 @@ use chrono::Local;
 
 fn main() {
 
+    //подключение к сокету 
     let mut stream = UnixStream::connect("/tmp/unix_sock").expect("connect error");
 
+    //отправка данных
     stream.write_all(b"SOCKET_CLIENT_ON_RUST_HI").expect("send error");
 
+    //получение данных от программы-сервера
     let mut buf = [0u8; 64];
     let message = stream.read(&mut buf).expect("recv error");
     let text = String::from_utf8_lossy(&buf[..message]);
 
+    // получение текущей даты и времени
     let now = Local::now().format("%d-%m-%Y %H:%M").to_string();
 
+    //открытите лог-файла и последующая запись с временем
     let log_file = OpenOptions::new()
         .create(true)
         .append(true)
